69d1589854eec68aa6a989971850dd6a4310d4ba
Axel-Ollivier <axel.ollivier56@outlook.com>
2022-04-21 15:30:59 +0200

    feat(unit-tests): flatten-tests, TempTracker tests

--- 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
+++ 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
@@ -4,8 +4,72 @@ use PHPUnit\Framework\TestCase;
 
 require_once('./src/TempTracker.php');
 
-class TempTrackerTest extends TestCase {
-    public function test_something() {
-        $this->assertEqual(42, 42);
+class TempTrackerTest extends TestCase
+{
+    public function test_insert()
+    {
+        $temp_tracker = new TempTracker();
+        $temp_tracker->insert(1);
+        $this->assertEquals([1], $temp_tracker->get_temps());
+    }
+
+    public function test_insert_out_of_range()
+    {
+        $temp_tracker = new TempTracker();
+        $this->expectException(ValueError::class);
+        $temp_tracker->insert(210);
+    }
+
+    public function test_insert_not_int()
+    {
+        $temp_tracker = new TempTracker();
+        $this->expectException(TypeError::class);
+        $temp_tracker->insert('1');
+    }
+
+    public function test_get_min()
+    {
+        $temp_tracker = new TempTracker();
+        $temp_tracker->insert(1);
+        $temp_tracker->insert(2);
+        $temp_tracker->insert(3);
+        $this->assertEquals(1, $temp_tracker->get_min());
+    }
+
+    public function test_get_min_no_values()
+    {
+        $temp_tracker = new TempTracker();
+        $this->assertEquals(-1, $temp_tracker->get_max());
+    }
+
+
+    public function test_get_max()
+    {
+        $temp_tracker = new TempTracker();
+        $temp_tracker->insert(1);
+        $temp_tracker->insert(2);
+        $temp_tracker->insert(3);
+        $this->assertEquals(3, $temp_tracker->get_max());
+    }
+
+    public function test_get_max_no_values()
+    {
+        $temp_tracker = new TempTracker();
+        $this->assertEquals(-1, $temp_tracker->get_max());
+    }
+
+    public function test_get_mean()
+    {
+        $temp_tracker = new TempTracker();
+        $temp_tracker->insert(1);
+        $temp_tracker->insert(2);
+        $temp_tracker->insert(3);
+        $this->assertEquals(2, $temp_tracker->get_mean());
+    }
+
+    public function test_get_mean_no_values()
+    {
+        $temp_tracker = new TempTracker();
+        $this->assertEquals(-1, $temp_tracker->get_max());
     }
 }
--- 1-intro/workshop/unit-tests/tests/flatten_test.php
+++ 1-intro/workshop/unit-tests/tests/flatten_test.php
@@ -6,8 +6,37 @@ require_once('./src/flatten.php');
 
 class FlattenTest extends TestCase
 {
-    public function test_something()
+    public function test_with_null()
     {
-        $this->assertEqual(1337, 1337);
+        $this->assertEquals([], flatten([]));
+    }
+
+    public function test_input_type(){
+        $this->expectException(TypeError::class);
+        flatten('1');
+        $this->expectException(TypeError::class);
+        flatten(1);
+        $this->expectException(TypeError::class);
+        flatten(true);
+    }
+
+    public function test_with_single_value()
+    {
+        $this->assertEquals([1], flatten([1]));
+    }
+
+    public function test_with_multiple_values()
+    {
+        $this->assertEquals([1, 2, 3], flatten([1, 2, 3]));
+    }
+
+    public function test_nested_once()
+    {
+        $this->assertEquals([1, 2, 3], flatten([1, [2, 3]]));
+    }
+
+    public function test_nested_multiple()
+    {
+        $this->assertEquals([1, 2, 3, 4, 5, 6, 7], flatten([[1, 2, 3], 4, [5, [6, 7]]]));
     }
 }
