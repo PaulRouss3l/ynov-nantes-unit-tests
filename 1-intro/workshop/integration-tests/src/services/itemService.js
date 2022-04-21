70dbfe51d99d8cd691c04e351c521ba05e0a6bdd
Simon Galoyan <simon.galoyan@ynov.com>
2022-04-21 17:17:15 +0200

    itemService (delete, update, getItem, deleteItems)

--- 1-intro/workshop/integration-tests/src/services/itemService.js
+++ 1-intro/workshop/integration-tests/src/services/itemService.js
@@ -12,6 +12,22 @@ const listItems = () => {
     return Item.find({});
 }
 
+const deleteItem = (name) => {
+    return Item.deleteOne({name: name});
+}
+
+const updateItem = (name, body) => {
+    return Item.updateOne({name: name}, body.updateName);
+}
+
+const getItem = (name) => {
+    return Item.findOne({name: name});
+}
+
+const deleteItems = () => {
+    return Item.deleteMany({});
+}
+
 module.exports = {
         createItem: createItem,
         listItems: listItems
