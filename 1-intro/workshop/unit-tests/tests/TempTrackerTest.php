1f443d340b00c381eda15406edb26023ba01e9f1
Yessi Munguengui <yessimunguengui@gmail.com>
2022-04-21 15:08:14 +0200

    feat: add temp tracker test | co-authored with Auriane Labille

--- 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
+++ 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
@@ -6,6 +6,24 @@ require_once('./src/TempTracker.php');
 
 class TempTrackerTest extends TestCase {
     public function test_something() {
-        $this->assertEqual(42, 42);
+        $this->assertEquals(42, 42);
+    }
+
+    public function test_array_is_empty() {
+        $tempTracker = new TempTracker();
+        $this->assertEmpty($tempTracker->get_temps());
+    }
+    
+    public function test_array_matches_value_inserted() {
+        $tempTracker = new TempTracker();
+        $tempTracker->insert(45);
+        $this->assertContains(45, $tempTracker->get_temps());
+    }
+    
+    public function test_limit_temps() {
+        $tempTracker = new TempTracker();
+        $tempTracker->insert(45,23,9,18,102);
+        $this->assertEquals(-10, $tempTracker->get_min());
+        $this->assertEquals(210, $tempTracker->get_max());
     }
 }
