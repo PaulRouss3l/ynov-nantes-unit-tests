beaf1a4db5ab681d5605bcbb7ad78b1d0c5cd3f9
thomas_mary <tmary@oui.sncf>
2022-04-21 16:59:08 +0200

    Ajout des tests unitaires

--- 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
+++ 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
@@ -6,6 +6,35 @@ require_once('./src/TempTracker.php');
 
 class TempTrackerTest extends TestCase {
     public function test_something() {
-        $this->assertEqual(42, 42);
+        $this->assertEquals(42, 42);
     }
+
+    public function test_is_value_added() {
+        $tempTracker = new TempTracker();
+        $tempTracker->insert(45);
+        $temps = (array)$tempTracker->get_temps();
+        $this->assertTrue(count($temps) > 0, "Tester que l'ajout a fonctionné");
+    }
+
+    public function test_max_and_lower_value() {
+        $tempTracker = new TempTracker();
+        $tempTracker->insert(45);
+        $tempTracker->insert(4);
+        $tempTracker->insert(0);
+        $tempTracker->insert(70);
+
+        $this->assertTrue($tempTracker->get_max() === 70 && $tempTracker->get_min() === 0);
+    }
+    
+    public function test_mean() {
+        $tempTracker = new TempTracker();
+        $tempTracker->insert(45);
+        $tempTracker->insert(4);
+        $tempTracker->insert(0);
+        $tempTracker->insert(70);
+        $mean = (45 + 0 + 4 + 70) / 4;
+
+        $this->assertTrue($tempTracker->get_mean() === $mean);
+    }
+
 }
--- 1-intro/workshop/unit-tests/tests/flatten_test.php
+++ 1-intro/workshop/unit-tests/tests/flatten_test.php
@@ -8,6 +8,34 @@ class FlattenTest extends TestCase
 {
     public function test_something()
     {
-        $this->assertEqual(1337, 1337);
+        $this->assertEquals(1337, 1337);
+    }
+    
+    public function test_if_return_type_is_array()
+    {
+        $testData = flatten([1, 2, [5,4,7], 4] );
+        $this->assertIsArray(
+            $testData,
+            "assert variable is array or not"
+        );
+    }
+
+    public function test_if_array_is_only_composed_of_int()
+    {
+        $testData = flatten([1, 2]);
+        $found = true;
+        foreach( $testData as $obj ) {
+            if( !is_int($obj)) {
+                $found = false;
+            }
+        }
+
+        $this->assertTrue( $found, "assert contains only int or not" );
+    }
+
+    public function test_if_array_is_not_only_composed_of_int()
+    {
+        $testData = flatten([1, 2, 'Bonjour', 3]);
+        $this->AssertNotContainsOnly('integer', $testData);
     }
 }
