16eb2f4b304cea68321daa60b339a8a506feaba5
Axel-Ollivier <axel.ollivier56@outlook.com>
2022-04-21 13:43:08 +0200

    feat(tests): test recherche formation

--- 1-intro/workshop/e2e-tests/tests/some_test.js
+++ 1-intro/workshop/e2e-tests/tests/some_test.js
@@ -3,3 +3,11 @@ Feature('Ynov Nantes');
 Scenario('Test Ynov Nantes Land Page', ({ I }) => {
     I.amOnPage('https://www.ynov-nantes.com/');
 });
+
+Scenario('Test Ynov Nantes Search ForBachelor Informatique', ({ I }) => {
+    I.amOnPage('https://www.ynov-nantes.com/');
+    I.click('a[href="/recherche/"]');
+    I.appendField('input[name="s"]', 'bachelor informatique');
+    I.wait(5);
+    I.see('Bachelor Informatique');
+});
