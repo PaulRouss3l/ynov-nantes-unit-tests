7c00a661135f44a8a50ca7d0066a7d4288d32943
Duchesne Quentin <quentin.duchesne@live.fr>
2022-04-21 14:38:25 +0200

    unit-test done

--- 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
+++ 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
@@ -6,6 +6,44 @@ require_once('./src/TempTracker.php');
 
 class TempTrackerTest extends TestCase {
     public function test_something() {
-        $this->assertEqual(42, 42);
+        $this->assertEquals(42, 42);
+    }
+
+    public function test_get_min() {
+        $tempTracker = new TempTracker();
+
+        $tempTracker->insert(105);
+        $tempTracker->insert(8);
+        $tempTracker->insert(76);
+        $tempTracker->insert(32);
+        $tempTracker->insert(3);
+        $tempTracker->insert(98);
+
+        $this->assertEquals(3, $tempTracker->get_min());
+    }
+
+    public function test_get_ax() {
+        $tempTracker = new TempTracker();
+
+        $tempTracker->insert(105);
+        $tempTracker->insert(8);
+        $tempTracker->insert(76);
+        $tempTracker->insert(32);
+        $tempTracker->insert(3);
+        $tempTracker->insert(98);
+
+        $this->assertEquals(105, $tempTracker->get_max());
+    }
+
+    public function test_get_mean() {
+        $tempTracker = new TempTracker();
+
+        $tempTracker->insert(0);
+        $tempTracker->insert(100);
+        $tempTracker->insert(50);
+
+        $expectedMean = 50;
+
+        $this->assertEquals($expectedMean, $tempTracker->get_mean());
     }
 }
--- 1-intro/workshop/unit-tests/tests/flatten_test.php
+++ 1-intro/workshop/unit-tests/tests/flatten_test.php
@@ -8,6 +8,21 @@ class FlattenTest extends TestCase
 {
     public function test_something()
     {
-        $this->assertEqual(1337, 1337);
+        $this->assertEquals(1337, 1337);
+    }
+
+    public function test_to_array()
+    {
+        $this->assertEquals([1,2,3,4], flatten([[1,2,[3]],4]));
+    }
+
+    public function test_basic_array()
+    {
+        $this->assertEquals([1,2,3,4],[1,2,3,4]);
+    }
+
+    public function test_empty_array()
+    {
+        $this->assertEquals([],[]);
     }
 }
