1bf21146438f36062a9d1e9ba8fd593dddfae75a
mariegautron <marigaut2@gmail.com>
2022-04-22 12:25:35 +0200

    start testing update quality function

--- 2-unit-tests/workshop/js/test/gilded_rose.test.js
+++ 2-unit-tests/workshop/js/test/gilded_rose.test.js
@@ -18,9 +18,31 @@ describe("Gilded Rose", function () {
   it("should have good attributes", function () {
     const _myShop = new Shop([...myShop.items, new Item("LeSuperNom", 50, 10)]);
     const items = _myShop.items;
+    const lastItem = items[items.length - 1];
 
-    expect(items[items.length - 1].name).toBe("LeSuperNom");
-    expect(items[items.length - 1].sellIn).toBe(50);
-    expect(items[items.length - 1].quality).toBe(10);
+    expect(lastItem.name).toBe("LeSuperNom");
+    expect(lastItem.sellIn).toBe(50);
+    expect(lastItem.quality).toBe(10);
+  });
+
+  it("should sellIn and quality decrease when update quality", () => {
+    const _myShop = new Shop([
+      new Item("Item1", 50, 10),
+      new Item("Item2", 80, 5),
+    ]);
+    const items = _myShop.updateQuality();
+    expect(items[0].sellIn).toBe(49);
+    expect(items[0].quality).toBe(9);
+
+    expect(items[1].sellIn).toBe(79);
+    expect(items[1].quality).toBe(4);
+  });
+
+  it("shoud quality decrease twice faster when sellIn in equal 0", () => {
+    const _myShop = new Shop([new Item("Item", 1, 80)]);
+    let items = _myShop.updateQuality();
+    expect(items[0].quality).toBe(79);
+    items = _myShop.updateQuality();
+    expect(items[0].quality).toBe(77);
   });
 });
