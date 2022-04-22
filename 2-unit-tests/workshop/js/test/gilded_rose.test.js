98321ef64b8fdb08201340a793a39af98e04e6a1
MaelFournier <mfournier2@la-joliverie.com>
2022-04-22 12:52:35 +0200

    add aged brie test [js]

--- 2-unit-tests/workshop/js/test/gilded_rose.test.js
+++ 2-unit-tests/workshop/js/test/gilded_rose.test.js
@@ -1,13 +1,13 @@
-const {Shop, Item} = require("../src/gilded_rose");
+const { Shop, Item } = require("../src/gilded_rose");
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
+describe("Gilded Rose", function () {
+  it("should foo", function () {
     const gildedRose = new Shop([new Item("Sword", 10, 5)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("Sword");
   });
 
-  it("quality shouldn't be negative", function() {
+  it("quality shouldn't be negative", function () {
     //sellIn = 5, quality = 3
     const gildedRose = new Shop([new Item("Sword", 5, 3)]);
 
@@ -18,5 +18,18 @@ describe("Gilded Rose", function() {
     }
 
     expect(gildedRose.items[0].quality).toBeGreaterThanOrEqual(0);
-  })
+  });
+
+  it("quality of Aged Brie has to increase", function () {
+    startQuality = 5
+    const gildedRose = new Shop([new Item("Aged Brie", 5, startQuality)]);
+
+    //Loop for 5 days
+    for (let i = 0; i < 5; i++) {
+      items = gildedRose.updateQuality();
+      console.log(items)
+    }
+
+    expect(gildedRose.items[0].quality).toBeGreaterThan(startQuality);
+  });
 });
