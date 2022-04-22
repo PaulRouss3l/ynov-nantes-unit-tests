d83936efaa7ac52f9084a6adaa064ad89d2c7db2
MaelFournier <mfournier2@la-joliverie.com>
2022-04-22 12:06:53 +0200

    test quality never negative [js]

--- 2-unit-tests/workshop/js/test/gilded_rose.test.js
+++ 2-unit-tests/workshop/js/test/gilded_rose.test.js
@@ -2,12 +2,21 @@ const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
   it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    const gildedRose = new Shop([new Item("Sword", 10, 5)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("foo");
+    expect(items[0].name).toBe("Sword");
   });
 
   it("quality shouldn't be negative", function() {
+    //sellIn = 5, quality = 3
+    const gildedRose = new Shop([new Item("Sword", 5, 3)]);
 
+    //Loop for 5 days
+    for (let i = 0; i < 5; i++) {
+      items = gildedRose.updateQuality();
+      //console.log(items)
+    }
+
+    expect(gildedRose.items[0].quality).toBeGreaterThanOrEqual(0);
   })
 });
