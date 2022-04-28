1d6e1221728a646c7602263e632b43172fb67fa6
Maxime Préard <maxime.preard@hotmail.fr>
2022-04-28 17:31:51 +0200

    fix bad merge

---
+++ 3-test-driven-development/workshop/php/test/GridTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GridTests;
+
+use MineSweeper\MineSweeper;
+use MineSweeper\Grid;
+use PHPUnit\Framework\TestCase;
+
+class GridTest extends TestCase
+{
+    private $grids; 
+
+    protected function setUp(): void
+    {
+        $this->grids = [
+            new Grid(4,4,3)
+        ];
+    } 
+
+    public function testCreationGrid(): void
+    {
+        /** @var Grid */
+        $grid = $this->grids[0];
+
+        $this->assertEquals($grid->getNbLine(), 4);
+        $this->assertEquals($grid->getNbCase(), 4);
+        $this->assertEquals($grid->getNbBomb(), 3);
+    }
+
+    public function testModificationGrid(): void
+    {
+        /** @var Grid */
+        $grid = $this->grids[0];
+
+        $grid->setNbLine(5);
+        $grid->setNbCase(5);
+        $grid->setNbBomb(5);
+
+        $this->assertEquals($grid->getNbLine(), 5);
+        $this->assertEquals($grid->getNbCase(), 5);
+        $this->assertEquals($grid->getNbBomb(), 5);
+    }
+
+    protected function tearDown(): void
+    {
+        unset($this->grids);
+    }
+} 
