9257825209402f0d0c2153876c21fb08016d9617
mariegautron <marigaut2@gmail.com>
2022-04-28 15:30:58 +0200

    others test

--- 3-test-driven-development/workshop/TDD/src/mind_sweeper.js
+++ 3-test-driven-development/workshop/TDD/src/mind_sweeper.js
@@ -1,32 +1,66 @@
 class Field {
-
-  createMineField (height, largeur){
-    return Array.from(Array(height), () => new Array(largeur).fill("."));
-  }
-
-  constructor(height, largeur) {
-    if (height >100) {
-      throw 'Height is too big';
+  checkErrors = (height, largeur) => {
+    if (height > 100) {
+      throw "Height is too big";
     }
-    if (largeur >100) {
-      throw 'Largeur is too big'
+    if (largeur > 100) {
+      throw "Largeur is too big";
     }
+
+    if (height <= 0) {
+      throw "Height is too low";
+    }
+
+    if (largeur <= 0) {
+      throw "Largeur is too low";
+    }
+  };
+
+  checkMindefield = (mindfield, height, width) => {
+    const tabField = this.transformMindfieldToTable(mindfield);
+
+    if (!tabField.flat().includes("*")) {
+      throw "Must be at least one bomb";
+    }
+
+    if (tabField.length !== height) {
+      throw "Height not correspond to mindefield";
+    }
+
+    tabField.forEach((ligne) => {
+      if (ligne.length !== width) {
+        throw "Width not correspond to mindefield";
+      }
+    });
+  };
+
+  transformMindfieldToTable = (mindfield) => {
+    const splitString = mindfield.split("\n");
+    const fieldTab = splitString.map((str) => str.replace(/ /g, ""));
+    console.log(fieldTab);
+
+    const superTable = fieldTab.map((ligne) => {
+      return Array.from(ligne);
+    });
+
+    console.log("superTable", superTable);
+
+    return superTable;
+  };
+
+  constructor(height, largeur, mindfield) {
+    this.checkErrors(height, largeur);
+    this.checkMindefield(mindfield, height, largeur);
     this.height = height;
     this.largeur = largeur;
-    this.mindfield = this.createMineField(height, largeur);
+    this.mindfield = mindfield;
   }
-
-}
-
-function BadValue(message = "") {
-  this.message = message;
 }
 
 class MindSweeper {
   constructor() {
     //TODO
   }
-  
 }
 
 module.exports = {
--- 3-test-driven-development/workshop/TDD/src/test/test.js
+++ 3-test-driven-development/workshop/TDD/src/test/test.js
@@ -1,34 +1,94 @@
-import { Field, userInput } from "../mind_sweeper";
+import {
+  field_one_bomb4x4,
+  field_one_bomb5x4,
+  field_only_points4x4,
+  field_only_points5x4,
+} from "../fields";
+import { Field } from "../mind_sweeper";
 
 describe("Minesweeper", function () {
-  it("field has good number of character", function () {
-      var field = new Field (5,4); 
-      //la ligne 1 doit avoir une longueur de 4
-      field.mindfield.forEach(line => {
-        expect(line.length).toBe(4);
-      });
+  // Tests for height and width field
+
+  it("height can't be too big", function () {
+    expect(() => {
+      new Field(50000, 4, field_only_points4x4);
+    }).toThrowError(/^Height is too big$/);
+  });
+
+  it("width can't be too big", function () {
+    expect(() => {
+      new Field(4, 5000, field_only_points4x4);
+    }).toThrowError(/^Largeur is too big$/);
+  });
+
+  it("height should not be 0 or less", function () {
+    expect(() => {
+      new Field(0, 5, field_only_points4x4);
+    }).toThrowError(/^Height is too low$/);
+  });
+
+  it("largeur should not be 0 or less", function () {
+    expect(() => {
+      new Field(5, 0, field_only_points4x4);
+    }).toThrowError(/^Largeur is too low$/);
+  });
+
+  it("height and width should correspond to the mindfield", () => {
+    const field = new Field(5, 4, field_one_bomb5x4);
+    const fieldToTable = field.transformMindfieldToTable(
+      field_one_bomb5x4,
+      5,
+      4
+    );
+
+    fieldToTable.forEach((ligne) => {
+      expect(ligne.length).toBe(4);
+    });
+
+    expect(fieldToTable.length).toBe(5);
+  });
+
+  it("should throw an error if height not corespond to mindefield", () => {
+    expect(() => {
+      const field = new Field(6, 4, field_one_bomb5x4);
+      field.transformMindfieldToTable(field_one_bomb5x4, 6, 4);
+    }).toThrowError(/^Height not correspond to mindefield$/);
+  });
+
+  it("should throw an error if width not corespond to mindefield", () => {
+    expect(() => {
+      const field = new Field(5, 3, field_one_bomb5x4);
+      field.transformMindfieldToTable(field_one_bomb5x4, 5, 3);
+    }).toThrowError(/^Width not correspond to mindefield$/);
   });
 
   it("field respect character", function () {
-    var field = new Field (5,4); 
-    field.mindfield.forEach(line => {
-      line.forEach (element => 
-        expect(element).toMatch(/^[\*|\.]*$/) )
-    }); 
-});
+    var field = new Field(5, 4, field_one_bomb5x4);
 
-it("height can't be too big", function () {
-  expect(() => {
-    new Field (50000,4)
-  }).toThrowError(/^Height is too big$/);
-});
+    const fieldToTable = field.transformMindfieldToTable(
+      field_one_bomb5x4,
+      5,
+      4
+    );
 
-it("width can't be too big", function () {
-  expect(() => {
-    new Field (4,5000)
-  }).toThrowError(/^Largeur is too big$/);
-});
+    fieldToTable.forEach((ligne) => {
+      const tableLigne = ligne;
+      tableLigne.forEach((element) => expect(element).toMatch(/^[\*|\.]*$/));
+    });
+  });
 
+  it("field has at least one bomb", function () {
+    var field = new Field(4, 4, field_one_bomb4x4);
 
+    const tabField = field.transformMindfieldToTable(field_one_bomb4x4, 4, 4);
+    const isBomb = tabField.flat().includes("*");
+    expect(isBomb).toBe(true);
+  });
 
+  it("should throw an error if not bomb in field", function () {
+    expect(() => {
+      var field = new Field(5, 4, field_only_points5x4);
+      field.transformMindfieldToTable(field_only_points5x4, 5, 4);
+    }).toThrowError(/^Must be at least one bomb$/);
+  });
 });
