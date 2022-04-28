37f5fdb75b35d9062d34a78ea1db548c5e87cd37
Marthy28 <titou.mary@gmail.com>
2022-04-28 13:24:16 +0200

    stucture

--- 3-test-driven-development/workshop/TDD/src/mind_sweeper.js
+++ 3-test-driven-development/workshop/TDD/src/mind_sweeper.js
@@ -1,9 +1,25 @@
 class Field {
-  constructor(hauteur, largeur) {
-    this.hauteur = hauteur;
-    this.largeur = largeur;
-    this.mindfield = ?
+
+  createMineField (height, largeur){
+    return Array.from(Array(height), () => new Array(largeur).fill("."));
   }
+
+  constructor(height, largeur) {
+    if (height >100) {
+      throw 'Height is too big';
+    }
+    if (largeur >100) {
+      throw 'Largeur is too big'
+    }
+    this.height = height;
+    this.largeur = largeur;
+    this.mindfield = this.createMineField(height, largeur);
+  }
+
+}
+
+function BadValue(message = "") {
+  this.message = message;
 }
 
 class MindSweeper {
--- 3-test-driven-development/workshop/TDD/src/test/test.js
+++ 3-test-driven-development/workshop/TDD/src/test/test.js
@@ -1,5 +1,34 @@
-import { userInput } from "../mind_sweeper";
+import { Field, userInput } from "../mind_sweeper";
 
 describe("Minesweeper", function () {
-  it("should display board game", function () {});
+  it("field has good number of character", function () {
+      var field = new Field (5,4); 
+      //la ligne 1 doit avoir une longueur de 4
+      field.mindfield.forEach(line => {
+        expect(line.length).toBe(4);
+      });
+  });
+
+  it("field respect character", function () {
+    var field = new Field (5,4); 
+    field.mindfield.forEach(line => {
+      line.forEach (element => 
+        expect(element).toMatch(/^[\*|\.]*$/) )
+    }); 
+});
+
+it("height can't be too big", function () {
+  expect(() => {
+    new Field (50000,4)
+  }).toThrowError(/^Height is too big$/);
+});
+
+it("width can't be too big", function () {
+  expect(() => {
+    new Field (4,5000)
+  }).toThrowError(/^Largeur is too big$/);
+});
+
+
+
 });
